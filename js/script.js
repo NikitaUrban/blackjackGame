87eadea912ec775eb96da8735d1301c5b6d114c1
nikitka <nick.urbanovich@mail.ru>
2020-09-09 23:54:11 +0300

    the end

--- js/script.js
+++ js/script.js
@@ -1,28 +1,30 @@
 let platBtn = $('#playBtn')//кнопка Играть!
 let moreBtn = $('#moreBtn')//кнопка Еще
 let enoughBtn = $('#enoughBtn')//Кнопка хватит
-let newGame = $('#newGame')
-
+let clear = $('#clear')//кнопка очистки
 let playerCards = $('.your_cards_block .card')//блок с картами игрока
 let dealerCards = $('.dealer_cards_block .card')//блок с картами диллера
 let dealerSum = 0 // сумма очков диллера
 let playerSum = 0 // сумма очков игрока
-let cards =
+let cards = // массив объектов наших карт с их весом и путями к ним
     [{weight:2,src:'img/2b.png'}, {weight:2,src:'img/2c.png'}, {weight:2,src:'img/2k.png'}, {weight:2,src:'img/2p.png'},
-    {weight:3,src:'img/3b.png'}, {weight:3,src:'img/3c.png'}, {weight:3,src:'img/3k.png'}, {weight:3,src:'img/3p.png'},
-    {weight:4,src:'img/4b.png'}, {weight:4,src:'img/4c.png'}, {weight:4,src:'img/4k.png'}, {weight:4,src:'img/4p.png'},
-    {weight:5,src:'img/5b.png'}, {weight:5,src:'img/5c.png'}, {weight:5,src:'img/5k.png'}, {weight:5,src:'img/5p.png'},
-    {weight:6,src:'img/6b.png'}, {weight:6,src:'img/6c.png'}, {weight:6,src:'img/6k.png'}, {weight:6,src:'img/6p.png'},
-    {weight:7,src:'img/7b.png'}, {weight:7,src:'img/7c.png'}, {weight:7,src:'img/7k.png'}, {weight:7,src:'img/7p.png'},
-    {weight:8,src:'img/8b.png'}, {weight:8,src:'img/8c.png'}, {weight:8,src:'img/8k.png'}, {weight:8,src:'img/8p.png'},
-    {weight:9,src:'img/9b.png'}, {weight:9,src:'img/9c.png'}, {weight:9,src:'img/9k.png'}, {weight:9,src:'img/9p.png'},
-    {weight:10,src:'img/10b.png'}, {weight:10,src:'img/10c.png'}, {weight:10,src:'img/10k.png'}, {weight:10,src:'img/10p.png'},
-    {weight:10,src:'img/jb.png'}, {weight:10,src:'img/jc.png'}, {weight:10,src:'img/jk.png'}, {weight:10,src:'img/jp.png'},
-    {weight:10,src:'img/db.png'}, {weight:10,src:'img/dc.png'}, {weight:10,src:'img/dk.png'}, {weight:10,src:'img/dp.png'},
-    {weight:10,src:'img/kb.png'}, {weight:10,src:'img/kc.png'}, {weight:10,src:'img/kk.png'}, {weight:10,src:'img/kp.png'},
-    {weight:11,src:'img/ab.png'}, {weight:11,src:'img/ac.png'}, {weight:11,src:'img/ak.png'}, {weight:11,src:'img/ap.png'}
+        {weight:3,src:'img/3b.png'}, {weight:3,src:'img/3c.png'}, {weight:3,src:'img/3k.png'}, {weight:3,src:'img/3p.png'},
+        {weight:4,src:'img/4b.png'}, {weight:4,src:'img/4c.png'}, {weight:4,src:'img/4k.png'}, {weight:4,src:'img/4p.png'},
+        {weight:5,src:'img/5b.png'}, {weight:5,src:'img/5c.png'}, {weight:5,src:'img/5k.png'}, {weight:5,src:'img/5p.png'},
+        {weight:6,src:'img/6b.png'}, {weight:6,src:'img/6c.png'}, {weight:6,src:'img/6k.png'}, {weight:6,src:'img/6p.png'},
+        {weight:7,src:'img/7b.png'}, {weight:7,src:'img/7c.png'}, {weight:7,src:'img/7k.png'}, {weight:7,src:'img/7p.png'},
+        {weight:8,src:'img/8b.png'}, {weight:8,src:'img/8c.png'}, {weight:8,src:'img/8k.png'}, {weight:8,src:'img/8p.png'},
+        {weight:9,src:'img/9b.png'}, {weight:9,src:'img/9c.png'}, {weight:9,src:'img/9k.png'}, {weight:9,src:'img/9p.png'},
+        {weight:10,src:'img/10b.png'}, {weight:10,src:'img/10c.png'}, {weight:10,src:'img/10k.png'}, {weight:10,src:'img/10p.png'},
+        {weight:10,src:'img/jb.png'}, {weight:10,src:'img/jc.png'}, {weight:10,src:'img/jk.png'}, {weight:10,src:'img/jp.png'},
+        {weight:10,src:'img/db.png'}, {weight:10,src:'img/dc.png'}, {weight:10,src:'img/dk.png'}, {weight:10,src:'img/dp.png'},
+        {weight:10,src:'img/kb.png'}, {weight:10,src:'img/kc.png'}, {weight:10,src:'img/kk.png'}, {weight:10,src:'img/kp.png'},
+        {weight:11,src:'img/ab.png'}, {weight:11,src:'img/ac.png'}, {weight:11,src:'img/ak.png'}, {weight:11,src:'img/ap.png'}
 
-]
+    ]
+let arrayImage = [] // массив картинок
+let playerCardsCounter = 2//счетчик для массива с картами игрока, чтобы можно было начать отсчет с элемента
+// с индексом 2
 
 shuffle = () => {//функция перемешивания колоды карт
     //в течение 1000 раз мы будем менять две карты в случайном месте в колоде
@@ -39,7 +41,7 @@ shuffle = () => {//функция перемешивания колоды кар
 }
 
 changeObjSrc = () =>{//меняет значение свойства src для каждого объекта на картинки
-    let arrayImage = [] // массив картинок
+
     for (let i = 0; i < cards.length; i++)
     {
         arrayImage [i] = new Image ()
@@ -48,12 +50,13 @@ changeObjSrc = () =>{//меняет значение свойства src для
         cards[i].src=tmp
     }
 }
+
+
 moreBtn.prop('disabled',true)
 enoughBtn.prop('disabled',true)
 
 
 platBtn.click( ()=>{
-
     platBtn.prop('disabled',true)
     changeObjSrc()
     shuffle()
@@ -78,27 +81,112 @@ platBtn.click( ()=>{
         }
     })
 
-    setTimeout(() =>{
-        moreBtn.prop('disabled',false)
-        enoughBtn.prop('disabled',false)
-    },2200)
-
     if (playerSum === 21){
         setTimeout(() =>{
-            alert('Игрок победил, у него 21!')
-            newGame.css('display','block')
-        },2100)
-    }
-    if (dealerSum === 21){
-        setTimeout(() =>{
-            alert('Диллер победил, у него 21!')
-            newGame.css('display','block')
+            alert('Вы победили, у вас 21!')
+            alert('Обновите страницу, чтобы сыграть заново!')
         },2100)
 
+    } else if (dealerSum === 21){
+        setTimeout(() =>{
+            alert('Диллер победил, у него 21!')
+            alert('Обновите страницу, чтобы сыграть заново!')
+            moreBtn.prop('disabled',true)
+            enoughBtn.prop('disabled',true)
+        },2100)
+
+    }else if (playerSum > 21){
+        setTimeout(() =>{
+            alert('У вас перебор ' + playerSum + ' очков, вы проиграли(')
+            alert('Обновите страницу, чтобы сыграть заново!')
+            moreBtn.prop('disabled',true)
+            enoughBtn.prop('disabled',true)
+        },2100)
+
+    }else if (dealerSum > 21){
+        setTimeout(() =>{
+            alert('У диллера перебор ' + dealerSum + ' очков, вы победили!')
+            alert('Обновите страницу, чтобы сыграть заново!')
+            moreBtn.prop('disabled',true)
+            enoughBtn.prop('disabled',true)
+        },2100)
+
+    }else {
+        setTimeout(() =>{
+            moreBtn.prop('disabled',false)
+            enoughBtn.prop('disabled',false)
+        },2200)
     }
+
+
 
 })
 
 moreBtn.click(function () {
+    $('.your_cards_block .card').eq(playerCardsCounter).css({'background':'none'}).append(cards[playerCardsCounter+2].src)
+    playerSum+=cards[playerCardsCounter+2].weight
+    if (playerSum === 21){
+        setTimeout(() =>{
+            alert('Вы победили, у вас 21!')
+            alert('Обновите страницу, чтобы сыграть заново!')
+            moreBtn.prop('disabled',true)
+            enoughBtn.prop('disabled',true)
+        },2000)
+    }else if (playerSum > 21){
+        alert('У вас перебор ' + playerSum + ' очков, вы проиграли(')
+        alert('Обновите страницу, чтобы сыграть заново!')
+        moreBtn.prop('disabled',true)
+        enoughBtn.prop('disabled',true)
+    }else if (playerCardsCounter === 4){
+        moreBtn.prop('disabled',true)
+    }else {
+        playerCardsCounter++
+    }
 
 })
+
+enoughBtn.click( () =>{
+    enoughBtn.prop('disabled',true)
+    moreBtn.prop('disabled',true)
+     if (dealerSum > playerSum){
+        alert('Диллер победил, у него ' + dealerSum + ' очков!')
+         alert('Обновите страницу, чтобы сыграть заново!')
+    }else if(dealerSum === playerSum){
+         alert('Ничья!')
+         alert('Обновите страницу, чтобы сыграть заново!')
+     } else {
+         for (let i = 2; i < 5 ; i++){
+             $('.dealer_cards_block .card').eq(i).css({'background': 'none'})
+                 .append(cards[i + 3].src)
+             dealerSum+=cards[i + 3].weight
+             if (dealerSum === 21){
+                 alert('Диллер победил, у него 21!')
+                 alert('Обновите страницу, чтобы сыграть заново!')
+                 break
+             }
+             if (dealerSum > 21){
+                 alert('У диллера перебор, ' + dealerSum + ' очков, вы победили!')
+                 clear.css({'display':'block'})
+                 break
+             }
+             if (dealerSum > playerSum){
+                 alert('Диллер победил, у него ' + dealerSum + ' очков!')
+                 alert('Обновите страницу, чтобы сыграть заново!')
+                 break
+             }
+             if (playerSum === dealerSum) {
+                 alert('Ничья!')
+                 alert('Обновите страницу, чтобы сыграть заново!')
+                 break
+             }
+             if (i === 4 && (playerSum > dealerSum)) {
+                 alert('Вы победили, у вас ' + playerSum + ' очков!')
+                 alert('Обновите страницу, чтобы сыграть заново!')
+                 break
+             }
+         }
+
+     }
+
+})
+
